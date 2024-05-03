8ee68d3b9545d08f5a4cdafd8d78e917ea558e97
ema <mccoy39082@163.com>
2024-05-03 13:35:45 +0800

    update: clean codes

--- ValueConverters.Shared/ReversibleValueToBoolConverterBase.cs
+++ ValueConverters.Shared/ReversibleValueToBoolConverterBase.cs
@@ -2,35 +2,34 @@
 using System.Globalization;
 using Avalonia;
 
-namespace ValueConverters
+namespace ValueConverters;
+
+public abstract class ReversibleValueToBoolConverterBase<T, TConverter> : ValueToBoolConverterBase<T, TConverter>
+    where TConverter : new()
 {
-    public abstract class ReversibleValueToBoolConverterBase<T, TConverter> : ValueToBoolConverterBase<T, TConverter>
-        where TConverter : new()
+    public abstract T FalseValue { get; set; }
+
+    public bool BaseOnFalseValue
     {
-        public abstract T FalseValue { get; set; }
-
-        public bool BaseOnFalseValue
-        {
-            get { return (bool)this.GetValue(BaseOnFalseValueProperty); }
-            set { this.SetValue(BaseOnFalseValueProperty, value); }
-        }
-
-        protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (!this.BaseOnFalseValue)
-            {
-                return base.Convert(value, targetType, parameter, culture);
-            }
-
-            var falseValue = this.FalseValue;
-            return !Equals(value, falseValue) ^ this.IsInverted;
-        }
-
-        protected override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return true.Equals(value) ^ this.IsInverted ? this.TrueValue : this.FalseValue;
-        }
-
-        public static readonly AvaloniaProperty BaseOnFalseValueProperty = PropertyHelper.Create<bool, ValueToBoolConverterBase<T, TConverter>>(nameof(BaseOnFalseValueProperty));
+        get => (bool)this.GetValue(BaseOnFalseValueProperty);
+        set => this.SetValue(BaseOnFalseValueProperty, value);
     }
+
+    protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (!this.BaseOnFalseValue)
+        {
+            return base.Convert(value, targetType, parameter, culture);
+        }
+
+        var falseValue = this.FalseValue;
+        return !Equals(value, falseValue) ^ this.IsInverted;
+    }
+
+    protected override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return true.Equals(value) ^ this.IsInverted ? this.TrueValue : this.FalseValue;
+    }
+
+    public static readonly AvaloniaProperty BaseOnFalseValueProperty = PropertyHelper.Create<bool, ValueToBoolConverterBase<T, TConverter>>(nameof(BaseOnFalseValueProperty));
 }
